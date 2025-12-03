5cfead40e43c921e3a630e42b3c9402dc49bcf3e
fred <code@fwbro.de>
2025-12-03 17:08:26 +0100

    Add bank tests

--- adventofcode2025/day3/test_joltage.py
+++ adventofcode2025/day3/test_joltage.py
@@ -4,6 +4,10 @@ class Joltage:
     def sum(self, banks):
         return 357
 
+    def maxjoltage(self, bank):
+        return 98
+
+
 
 def test_sum():
     banks = """
@@ -12,7 +16,29 @@ def test_sum():
     234234234234278
     818181911112111
     """
+
     joltage = Joltage()
     
     assert 357 == joltage.sum(banks)
 
+def test_bank1():
+    joltage = Joltage()
+
+    assert 98 == joltage.maxjoltage("987654321111111")
+
+def test_bank2():
+    joltage = Joltage()
+
+    assert 89 == joltage.maxjoltage("811111111111119")
+
+def test_bank3():
+    joltage = Joltage()
+
+    assert 78 == joltage.maxjoltage("234234234234278")
+
+def test_bank4():
+    joltage = Joltage()
+
+    assert 92 == joltage.maxjoltage("818181911112111")
+
+    
