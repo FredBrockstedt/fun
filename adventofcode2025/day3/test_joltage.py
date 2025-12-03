17659f4c40b73218ceafa16defd042b017e11bd5
fred <code@fwbro.de>
2025-12-03 17:27:00 +0100

    Solution for day 3 part 1

--- adventofcode2025/day3/test_joltage.py
+++ adventofcode2025/day3/test_joltage.py
@@ -2,24 +2,32 @@
 
 class Joltage:
     def sum(self, banks):
-        return 357
+        "Return the sum of joltages produced by banks parameter"
+        return sum([self.maxjoltage(bank) for bank in banks])
 
     def maxjoltage(self, bank):
-        return 98
+        "Returns the maximum of joltage a bank of batteries can produce"
 
+        # find the largest number aslong as it is not the last
+        # and its position in the bank
+        left = max(list(bank[:-1]))
+        left_index = bank.index(str(left))
 
+        # we want to find the second number
+        right = max(list(bank[left_index+1:]))
+        
+        return 10 * int(left) + int(right)
+
+################################################################################
+# pytest
 
 def test_sum():
-    banks = """
-    987654321111111
-    811111111111119
-    234234234234278
-    818181911112111
-    """
+    banks = "987654321111111\n811111111111119\n234234234234278\n818181911112111"
+
 
     joltage = Joltage()
     
-    assert 357 == joltage.sum(banks)
+    assert 357 == joltage.sum(banks.split("\n"))
 
 def test_bank1():
     joltage = Joltage()
@@ -42,3 +50,17 @@ def test_bank4():
     assert 92 == joltage.maxjoltage("818181911112111")
 
     
+################################################################################
+# main
+
+def main():
+    with open("data.txt", "r") as f:
+        lines = f.read().splitlines()
+
+    j = Joltage()
+
+    joltage = j.sum(lines)
+    print(f"Joltage = {joltage}")
+
+if __name__ == '__main__':
+    main()
